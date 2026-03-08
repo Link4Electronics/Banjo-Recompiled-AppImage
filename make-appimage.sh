5f58bfc7a1b195fb83a0b235623e5a35aae62eb8
Link4Electronics <link4electronics@gmail.com>
2026-03-08 06:31:47 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,9 +9,10 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/BanjoRecomp/BanjoRecomp/refs/heads/main/icons/app.png
 export DEPLOY_OPENGL=1
+export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-quick-sharun /opt/banjorecomp/BanjoRecompiled
+quick-sharun ./AppDir/bin/BanjoRecompiled
 
 # Additional changes can be done in between here
 
